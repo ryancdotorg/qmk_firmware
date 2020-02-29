7d5ca2bbdae4c00e6e894188ff7d8618de1af16d
Ryan Castellucci <code@ryanc.org>
2020-02-29 16:05:20 +0000

    Set up ryancdotorg userspace.

---
+++ users/ryancdotorg/ryancdotorg.c
@@ -0,0 +1,3 @@
+/* Copyright (c) 2020 Ryan Castellucci @ryancdotorg GPLv2+ */
+#include QMK_KEYBOARD_H
+#include "ryancdotorg.h"
---
+++ users/ryancdotorg/ryancdotorg.h
@@ -0,0 +1,2 @@
+/* Copyright (c) 2020 Ryan Castellucci @ryancdotorg GPLv2+ */
+#pragma once
